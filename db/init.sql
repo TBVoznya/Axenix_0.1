a527a6fb3fa0d1680f6b6990520fcb801dee4a64
Yaro Bor <borovinscky.yaroslaw@yandex.ru>
2025-04-05 18:10:31 +0300

    Add finished BD.

--- db/init.sql
+++ db/init.sql
@@ -1,77 +1,59 @@
-CREATE TABLE tarifs (
+-- Таблица типов продуктов
+CREATE TABLE product_types (
+    type_id SERIAL PRIMARY KEY,
+    type_name TEXT UNIQUE NOT NULL
+);	
+
+-- Заполнение таблицы типами продуктов
+INSERT INTO product_types (type_name)
+VALUES
+    ('Банка'),
+    ('Фрукт'),
+    ('Выпечка');
+
+-- Таблица продуктов
+CREATE TABLE products (
     id SERIAL PRIMARY KEY,
-    name_tarif TEXT NOT NULL,
-    inter_packet_mb INTEGER NOT NULL,
-    cost_tarif MONEY NOT NULL
+    name_product TEXT NOT NULL,
+    cost_product MONEY NOT NULL,
+	type_id INT NOT null,
+	FOREIGN KEY (type_id) 
+  REFERENCES product_types(type_id)
 );
 
-INSERT INTO tarifs (name_tarif, inter_packet_mb, cost_tarif)
+-- Заполнение таблицы продуктов
+INSERT INTO products (name_product, cost_product, type_id)
 VALUES
-    ('Максимальный', 1000000, 800.00),
-    ('Мощный', 100, 400.00),
-    ('Честный', 10, 100.00);
-	
-		
-		
-		
-		
-CREATE TABLE services (
-    id SERIAL PRIMARY KEY,
-    name_service TEXT NOT NULL,
-    cost_service MONEY NOT NULL
-);
+    ('Адреналин', 90.00, 1),
+    ('Яблоко', 400.00, 2),
+    ('Булка', 100.00, 3);
 
-INSERT INTO services (name_service, cost_service)
-VALUES
-    ('АнтиВирус Касперский', 100.00),
-    ('Выделенный IP', 100.00),
-    ('Персональный менеджер', 100.00),
-    ('Фирменный роутер', 100.00);
-	
-	
-	
-	
-	
-	
 
-CREATE TABLE customers (
-    id SERIAL PRIMARY KEY,
-    num_dog TEXT CHECK (num_dog ~ '^516\d{6}$'),
-    fio TEXT NOT NULL,
-    telephone VARCHAR(20) DEFAULT NULL,
-    mail VARCHAR(100) DEFAULT NULL,
-	telegram_id TEXT DEFAULT NULL
+-- Таблица с координатами объектов (например полок или человечков)
+CREATE TABLE obj_location (
+    obj_id SERIAL PRIMARY KEY,
+    x_cord FLOAT NOT NULL,
+    y_cord FLOAT NOT NULL
 );
 
 
-INSERT INTO customers (num_dog, fio, telephone, mail)
-VALUES
-    (516123456, 'Иванов Иван Иванович', '+79001234567', 'ivanov@mail.ru'),
-    (516789012, 'Петров Петр Петрович', '+79112345678', 'petrov@gmail.com'),
-    (516345678, 'Сидорова Мария Ивановна', '+79207654321', 'sidorova@yandex.ru'),
-    (516901234, 'Кузнецов Алексей Владимирович', '+79301234567', 'kuznetsov@mail.ru'),
-    (516567890, 'Смирнова Елена Николаевна', '+79401234567', 'smirnova@gmail.com'),
-    (516123789, 'Соколов Дмитрий Андреевич', '+79501234567', 'sokolov@yandex.ru'),
-    (516456789, 'Михайлова Анна Сергеевна', '+79601234567', 'mikhaylova@mail.ru'),
-    (516789456, 'Новиков Игорь Владимирович', '+79701234567', 'novikov@gmail.com'),
-    (516012345, 'Федорова Ольга Михайловна', '+79801234567', 'fedorova@yandex.ru');
-	
-	
-
-
-
-
-CREATE TABLE admins (
-    id SERIAL PRIMARY KEY,
-    login TEXT CHECK (login ~ '^a\w+$'),
-    fio TEXT NOT NULL,
-    telephone VARCHAR(20),
-    mail VARCHAR(100),
-	telegram_id TEXT DEFAULT NULL
+-- Таблица того чем наполнен объект
+CREATE TABLE obj_content (
+    id_content SERIAL PRIMARY KEY,
+	name_obj INT NOT NULL,
+    on_shelf INT DEFAULT NULL,
+	count_on INT DEFAULT 0,
+	FOREIGN KEY (on_shelf) REFERENCES products(id),
+	FOREIGN KEY (name_obj) REFERENCES obj_location(obj_id)
 );
 
-
-INSERT INTO admins (login, fio, telephone, mail)
-VALUES
-    ('admin1', 'Иванов Иван Иванович', '+79001234567', 'admin1@example.com'),
-    ('admin2', 'Петров Петр Петрович', '+79112345678', 'admin2@example.com');
+-- Таблица длинн путей до точек интереса
+CREATE TABLE lens (
+    id_content SERIAL PRIMARY KEY,
+	name_obj INT NOT NULL,
+    x_fin FLOAT DEFAULT NULL,
+	y_fin FLOAT DEFAULT NULL,
+	FOREIGN KEY (name_obj) REFERENCES obj_location(obj_id)
+);
+	
+		
