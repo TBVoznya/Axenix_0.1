aa952c8bc88648173ab1ceb76d9cbf212284a59c
Abdulali-coll <boomdoom22338@gmail.com>
2025-04-06 06:46:27 +0300

    targets NPC

--- Spring/src/main/java/com/hack/Spring/model/ObjLocation.java
+++ Spring/src/main/java/com/hack/Spring/model/ObjLocation.java
@@ -1,48 +1,36 @@
 package com.hack.Spring.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "obj_location")
 public class ObjLocation {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long objId;
-
-    public Long getObjId() {
-        return objId;
-    }
-    public void setObjId(Long objId) {
-        this.objId = objId;
-    }
+    
     private double xCord;
-
-    public double getxCord() {
-        return xCord;
-    }
-    public void setxCord(double xCord) {
-        this.xCord = xCord;
-    }
     private double yCord;
 
-    public double getyCord() {
-        return yCord;
-    }
-    public void setyCord(double yCord) {
+    public ObjLocation() {}
+
+    public ObjLocation(double xCord, double yCord) {
+        this.xCord = xCord;
         this.yCord = yCord;
     }
-    public int getYCord() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getYCord'");
-    }
-    public int getXCord() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getXCord'");
-    }
 
+    // геттеры и сеттеры
+    public Long getObjId() { return objId; }
+    public void setObjId(Long objId) { this.objId = objId; }
+
+    public double getXCord() { return xCord; }
+    public void setXCord(double xCord) { this.xCord = xCord; }
+
+    public double getYCord() { return yCord; }
+    public void setYCord(double yCord) { this.yCord = yCord; }
 }
